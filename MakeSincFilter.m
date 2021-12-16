93c03ed5318b9e22ac0654ece4fee9c319951dbd
Antonio Bassi <antoniovitor.gb@gmail.com>
2021-12-16 00:09:53 -0300

    Modifications on MakeSincFilter.m

--- MakeSincFilter.m
+++ MakeSincFilter.m
@@ -1,12 +1,12 @@
 function [H] = MakeSincFilter( df, B, Fs, Type )
     %   MAKESINCFILTER builds a FIR sinc lowpass filter.
-    %   [H] = MakeSincFilter( df, B, Fs )
+    %   [H] = MakeSincFilter( df, B, Fs, Type )
     %       H : Sinc Filter Coefficients (a.k.a Filter Kernel)
     %   Returns an array containing coefficients for rectangular windowed Sinc Filter
     %       df : Transition Bandwidth in Hz
     %       B  : Filter Bandwidth
     %       Fs : Sampling Frequency
-    %       Type : Filter Type string, that is Low, High, Band and Stop.
+    %       Type : Filter Type string, that is 'Low', 'High', 'Band' and 'Stop'.
     % -----------------------------------------------------------------------------------------
     %  file     : MakeSincFilter.m
     %  author   : Antonio Vitor-Grossi Basssi
@@ -33,23 +33,27 @@ function [H] = MakeSincFilter( df, B, Fs, Type )
         dd(W == 1e-9) = 1;
         H = dd - H;
     elseif(strcmp( Type, 'Band' ))
+        % Compute Cutoff frequencies
         Fc1 = B / Fs;
-        Fc2 = ( 2 * B ) / Fs;
+        Fc2 = ( 8 * B ) / Fs;
 
+        % Compute Dirac
         dd = zeros( size(W) );
         dd(W == 1e-9) = 1;
 
+        % Compute Highpass Transform
         H = sinc( 2 * Fc1 * W );
         H = H ./ sum( H );
-
         Hhp = dd - H;
 
+        % Compute Lowpass
         H = sinc( 2 * Fc2 * W );
         H = H ./ sum( H );
-
         Hlp = H;
 
+        % Bandpass Transformation
         H = conv(Hlp, Hhp);
+
     elseif(strcmp( Type, 'Stop' ))
     else
     end
